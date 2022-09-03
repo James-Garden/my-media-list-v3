b2ccb9a855806ae3f4cf17b3ae8255ca9bc6436e
James <jamesegarden@outlook.com>
2022-09-03 13:43:24 +0100

    setup page layout (#2)
    * setup page layout
    * added basic login page

--- accounts/views.py
+++ accounts/views.py
@@ -4,3 +4,6 @@ from django.contrib.auth import views as auth_views
 
 class LoginView(auth_views.LoginView):
     template_name = "accounts/login.html"
+    extra_context = {
+        'page_title': "Login"
+    }
--- my_media_list/settings.py
+++ my_media_list/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'design_system',
     'accounts',
     'compressor',
     'django.contrib.admin',
