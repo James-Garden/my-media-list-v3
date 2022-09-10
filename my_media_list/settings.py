cd64cbdde8f7cb7b3001646bc7e8c4f57f3dbcf6
James Garden <jamesegarden@outlook.com>
2022-09-10 19:15:36 +0100

    changed secret key to default to environ if available

--- my_media_list/settings.py
+++ my_media_list/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k!zsnk=q)u9_eg_)i3d+2)2xdm5ui!_t^sw9pf$4wzwgo8kv9k'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-k!zsnk=q)u9_eg_)i3d+2)2xdm5ui!_t^sw9pf$4wzwgo8kv9k')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
