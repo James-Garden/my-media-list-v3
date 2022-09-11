00a3e89c2b80bed1f9ea93bd5d5e10d1efd5a017
James Garden <jamesegarden@outlook.com>
2022-09-11 21:43:41 +0100

    added methods to get and add related media, RelatedMedia model and tests for all the methods

--- media/admin.py
+++ media/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from media.models import Media, Film, Series, Book
+
+admin.site.register(Media)
+admin.site.register(Film)
+admin.site.register(Series)
+admin.site.register(Book)
